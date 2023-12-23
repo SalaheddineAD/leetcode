3ef96845e8177414395177e8859c5c40a9681333
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2023-12-23 00:35:57 -0800

    Time: 31 ms (93.37%), Space: 17.4 MB (23.06%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,11 +1,10 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
         nb_weeks = n//7
-        nb_days_in_last_week = n%7
-        total=0
-        for i in range(nb_weeks):
-            total += (7+i)*(7+1+i)/2 - i*(i+1)/2
+        nb_last_days = n%7
         
-        total += (nb_days_in_last_week+nb_weeks)*(nb_days_in_last_week +nb_weeks 
-                                                  +1)/2 - (nb_weeks*(nb_weeks+1)/2)
-        return int(total)
+        money_from_complete_weeks = 28*nb_weeks + nb_weeks*(nb_weeks-1)*7/2
+        
+        money_from_last_days = (nb_weeks)*nb_last_days + nb_last_days*(nb_last_days+1)/2
+        
+        return int(money_from_complete_weeks + money_from_last_days)

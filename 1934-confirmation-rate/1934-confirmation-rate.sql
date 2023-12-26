74912e3687bf13c376d26a5299aed034ec768031
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2023-12-25 16:46:23 -0800

    Time: 1069 ms (93.87%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,9 +1,8 @@
 SELECT s.user_id, 
-ROUND(avg(if(c.action="confirmed",1,0)),2)
-# ROUND(COUNT(if(c.action="confirmed",1,0))/COUNT(*),2) 
+# ROUND(avg(if(c.action="confirmed",1,0)),2)
+ROUND(SUM(if(c.action="confirmed",1,0))/COUNT(*),2) 
 AS confirmation_rate
 FROM signups s 
 LEFT JOIN confirmations c
 on s.user_id = c .user_id
 GROUP BY s.user_id
-# avg(if(c.action="confirmed",1,0))

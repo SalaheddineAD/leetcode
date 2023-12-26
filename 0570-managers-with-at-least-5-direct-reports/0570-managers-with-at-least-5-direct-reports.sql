efc798c7c341915328e6c1733cfa28e351a5c9db
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2023-12-25 16:20:18 -0800

    Time: 579 ms (52.20%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,8 @@
-WITH managers as(
-    SELECT managerId, count(managerId) as countManagers
-    FROM employee
-    GROUP BY managerId
-)  
 SELECT name 
 FROM employee
 WHERE id IN (
 SELECT managerId
-FROM managers
-WHERE countManagers>=5
+FROM employee
+    GROUP BY managerId 
+    HAVING COUNT(*) >= 5
 )

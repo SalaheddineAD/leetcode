5476cca75f4f3d93ea5451c9446cb64419730d32
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2023-12-29 23:37:41 -0800

    Time: 261 ms (18.58%), Space: 18.9 MB (8.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,6 +7,5 @@ class Solution:
         for i in range(len(nums)):
             if nums[i]==0:
                 nb_zeros +=1
-            elif nb_zeros >0:
-                nums[i-nb_zeros]=nums[i]
-                nums[i]=0
+            else:
+                nums[i-nb_zeros], nums[i] = nums[i], nums[i-nb_zeros]

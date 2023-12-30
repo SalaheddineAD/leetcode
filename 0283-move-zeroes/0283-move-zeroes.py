5902729e1cd60e6d678aedbc9553e2ccd14ecb93
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2023-12-29 23:39:42 -0800

    Time: 297 ms (18.25%), Space: 18.9 MB (8.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nb_zeros = 0
-        for i in range(len(nums)):
-            if nums[i]==0:
-                nb_zeros +=1
-            else:
-                nums[i-nb_zeros], nums[i] = nums[i], nums[i-nb_zeros]
+        # nb_zeros = 0
+        # for i in range(len(nums)):
+        #     if nums[i]==0:
+        #         nb_zeros +=1
+        #     else:
+        #         nums[i-nb_zeros], nums[i] = nums[i], nums[i-nb_zeros]
+        n = len(nums)
+        i = 0
+        while i < n:
+            if nums[i] == 0:
+                nums.pop(i)
+                nums.append(0)
+                # print(nums)
+                n -= 1
+                continue
+            i += 1

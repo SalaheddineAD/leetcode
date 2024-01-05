b34c53819bad96092889595076db3c4c0efa0a90
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2024-01-04 18:19:59 -0800

    Time: 429 ms (95.61%), Space: 17.9 MB (19.57%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestOnes(self, A, K):
+        i = 0
+        for j in range(len(A)):
+            K -= 1 - A[j]
+            if K < 0:
+                K += 1 - A[i]
+                i += 1
+        return j - i + 1

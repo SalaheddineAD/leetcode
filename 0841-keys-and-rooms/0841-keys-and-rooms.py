c5ec95a979f4275bfc403bea10eb65477c58c8af
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2024-01-13 14:54:44 -0800

    Time: 70 ms (58.07%), Space: 17.9 MB (21.85%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -2,12 +2,21 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         #we can perceive the key from room a to room b as an edge 
         
+#         n = len(rooms)
+#         open = set()
+
+#         def dfs(i):
+#             open.add(i)
+#             for j in rooms[i]:
+#                 if j not in open:
+#                     dfs(j)
+#         dfs(0)
+#         return len(open) == n
+    
         visited = []
-        result = 0
         
         def dfs(room : int):
-            nonlocal visited, rooms, result
-            result+=1
+            # nonlocal visited, rooms, result
             visited.append(room)
             for neighbor in rooms[room]:
                 if neighbor in visited:
@@ -15,5 +24,5 @@ class Solution:
                 dfs(neighbor)
         
         dfs(0)   
-        return result == len(rooms)
+        return len(visited) == len(rooms)
         

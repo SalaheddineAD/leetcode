7effa9be78a0771f52ed8393a69be482e009e90d
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2024-02-02 20:31:51 -0800

    Time: 705 ms (97.17%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,3 +1,16 @@
-select distinct product_id, 10 as price from Products where product_id not in(select distinct product_id from Products where change_date <='2019-08-16' )
-union 
-select product_id, new_price as price from Products where (product_id,change_date) in (select product_id , max(change_date) as date from Products where change_date <='2019-08-16' group by product_id)
+WITH recent_products AS(
+SELECT product_id, new_price AS price,
+    ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY change_date DESC) as rn
+FROM products
+    WHERE change_date <= "2019-08-16"
+),
+
+distinct_prods AS (
+SELECT DISTINCT product_id
+FROM products
+)
+
+SELECT d.product_id, IFNULL(r.price,10) AS price
+FROM distinct_prods d
+LEFT JOIN recent_products r
+ON d.product_id = r.product_id AND  r.rn =1

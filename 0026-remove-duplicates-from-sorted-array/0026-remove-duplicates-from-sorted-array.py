168bea99d13d6086182a5b65768bfee4a23ce89b
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2024-10-24 13:01:29 -0500

    Time: 60 ms (92.36%), Space: 17.9 MB (52.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,7 +3,6 @@ class Solution:
         last_element = nums[0]
         i=1
         while i < len(nums):
-            print(str(i)+", "+str(nums[i])+ ", "+str(last_element))
             if nums[i]==last_element:
                 nums.pop(i)
             else:

018becb4d37089abed0b7de5a00671a119d0d7dc
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2024-11-04 15:00:09 -0600

    Time: 7 ms (96.08%), Space: 22.7 MB (85.30%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,12 +1,8 @@
 class Solution:
-    def canReach(self, arr: List[int], start: int) -> bool:
-        # targets = [i for i,j in enumerate(arr) if j ==0]
-        # len_target = len(targets)
-        
+    def canReach(self, arr: List[int], start: int) -> bool:        
         n = len(arr)
         neighbors = deque([start])
         visited = set()
-        result = []
         
         while len(neighbors)!=0:
             curr = neighbors.pop()
@@ -21,9 +17,6 @@ class Solution:
                 potential_neighbor = curr+i*arr[curr]
                 if 0<=potential_neighbor<n and potential_neighbor not in visited:
                     neighbors.append(potential_neighbor)
-        # print(visited)
-        # if len(result)==len_target:
-        #     return True
         return False
 
             

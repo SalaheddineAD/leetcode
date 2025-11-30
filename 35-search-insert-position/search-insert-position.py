525f4fda57882fc71ca7421d558930932aacb155
Salah Eddine Ait Daoud <93080778+SalaheddineAD@users.noreply.github.com>
2025-11-29 20:34:31 -0600

    Time: 0 ms (100.00%) | Memory: 18.7 MB (26.15%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -9,11 +9,9 @@ class Solution:
             return 0 if target<=nums[0] else 1
 
         while step>=1:
-            print(f"pos = {pos}")
             if nums[pos] == target:
                 return pos
             pos = min(pos +step,size-1) if target>nums[pos] else max(pos-step,0)
-            print(f"step = {step}")
             step = int(step/2)
 
         if nums[size-1]<target:
